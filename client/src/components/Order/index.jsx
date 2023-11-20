c192ba4d6679406dbda3c9120c8bcbbd160b83e9
ChooSeongWoo <sldoal2423@gmail.com>
2023-11-20 19:13:45 +0900

    Order 퍼블리싱

--- client/src/components/Order/index.jsx
+++ client/src/components/Order/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 import * as S from "./style";
 
+import 꼬깔콘 from "../../assets/Img/Menu/꼬깔콘.png";
+
 const Order = () => {
   return (
     <S.Layout>
-      <S.OrderImg src="" />
+      <S.OrderImg src={꼬깔콘} />
+      <S.OrderName>꼬깔콘</S.OrderName>
+      <S.OrderCount>1</S.OrderCount>
     </S.Layout>
   );
 };
--- client/src/components/Order/style.js
+++ client/src/components/Order/style.js
@@ -1,6 +1,32 @@
 import styled from "styled-components";
 
-export const Layout = styled.div``;
+export const Layout = styled.div`
+  width: 22vw;
+  background-color: white;
+  border: 3px solid #dedede;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 10px;
+`;
 
-export const OrderImg = styled.div``;
+export const OrderImg = styled.img`
+  width: 6vw;
+`;
 
+export const OrderName = styled.p`
+  font-weight: 600;
+`;
+
+export const OrderCount = styled.p`
+  border: 3px solid #dedede;
+  width: 40px;
+  height: 40px;
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 400;
+  border-radius: 10px;
+  margin-right: 10px;
+`;
