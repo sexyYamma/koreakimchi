7c927b031ba5d71f1e4f8dbdaef0032907de07cf
ChooSeongWoo <sldoal2423@gmail.com>
2023-12-04 02:08:54 +0900

    Menu get 해서 갖다박음

--- client/src/Data/Menu.js
+++ client/src/Data/Menu.js
@@ -1,75 +1,78 @@
-import 꼬깔콘 from "../assets/Img/Menu/꼬깔콘.png";
-import 도리토스 from "../assets/Img/Menu/도리토스.png";
-import 밀키스 from "../assets/Img/Menu/밀키스.png";
-import 새우깡 from "../assets/Img/Menu/새우깡.png";
-import 신짱 from "../assets/Img/Menu/신짱.png";
-import 양파링 from "../assets/Img/Menu/양파링.png";
-import 칠성사이다 from "../assets/Img/Menu/칠성사이다.png";
-import 코카콜라 from "../assets/Img/Menu/코카콜라.png";
-import 파워에이드 from "../assets/Img/Menu/파워에이드.png";
-import 포카리스웨트 from "../assets/Img/Menu/포카리스웨트.png";
+import axios from "axios";
 
-const MenuData = [
-  {
-    MenuImg: 새우깡,
-    MenuName: "새우깡",
-    MenuPrice: 2000,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 꼬깔콘,
-    MenuName: "꼬깔콘",
-    MenuPrice: 2000,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 도리토스,
-    MenuName: "도리토스",
-    MenuPrice: 2000,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 양파링,
-    MenuName: "양파링",
-    MenuPrice: 2000,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 신짱,
-    MenuName: "신짱",
-    MenuPrice: 2000,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 포카리스웨트,
-    MenuName: "포카리스웨트",
-    MenuPrice: 1500,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 파워에이드,
-    MenuName: "파워에이드",
-    MenuPrice: 1500,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 코카콜라,
-    MenuName: "코카콜라",
-    MenuPrice: 1500,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 칠성사이다,
-    MenuName: "칠성사이다",
-    MenuPrice: 1500,
-    MenuCount: 3,
-  },
-  {
-    MenuImg: 밀키스,
-    MenuName: "밀키스",
-    MenuPrice: 1500,
-    MenuCount: 3,
-  },
-];
+let MenuData = [{}];
+const fetchData = async () => {
+  try {
+    const data = await axios.get("http://10.129.57.197:5000/snacks");
+
+    MenuData = [
+      {
+        MenuCount: data[0].MenuCount,
+        MenuId: data[0].MenuId,
+        MenuName: data[0].MenuName,
+        MenuPrice: data[0].MenuPrice,
+      },
+      {
+        MenuCount: data[1].MenuCount,
+        MenuId: data[1].MenuId,
+        MenuName: data[1].MenuName,
+        MenuPrice: data[1].MenuPrice,
+      },
+      {
+        MenuCount: data[2].MenuCount,
+        MenuId: data[2].MenuId,
+        MenuName: data[2].MenuName,
+        MenuPrice: data[2].MenuPrice,
+      },
+      {
+        MenuCount: data[3].MenuCount,
+        MenuId: data[3].MenuId,
+        MenuName: data[3].MenuName,
+        MenuPrice: data[3].MenuPrice,
+      },
+      {
+        MenuCount: data[4].MenuCount,
+        MenuId: data[4].MenuId,
+        MenuName: data[4].MenuName,
+        MenuPrice: data[4].MenuPrice,
+      },
+      {
+        MenuCount: data[5].MenuCount,
+        MenuId: data[5].MenuId,
+        MenuName: data[5].MenuName,
+        MenuPrice: data[5].MenuPrice,
+      },
+      {
+        MenuCount: data[6].MenuCount,
+        MenuId: data[6].MenuId,
+        MenuName: data[6].MenuName,
+        MenuPrice: data[6].MenuPrice,
+      },
+      {
+        MenuCount: data[7].MenuCount,
+        MenuId: data[7].MenuId,
+        MenuName: data[7].MenuName,
+        MenuPrice: data[7].MenuPrice,
+      },
+      {
+        MenuCount: data[8].MenuCount,
+        MenuId: data[8].MenuId,
+        MenuName: data[8].MenuName,
+        MenuPrice: data[8].MenuPrice,
+      },
+      {
+        MenuCount: data[9].MenuCount,
+        MenuId: data[9].MenuId,
+        MenuName: data[9].MenuName,
+        MenuPrice: data[9].MenuPrice,
+      },
+    ];
+
+    return MenuData;
+  } catch (error) {
+    console.error("데이터를 가져오는 도중 오류가 발생했습니다:", error);
+    return [];
+  }
+};
 
 export default MenuData;
--- client/src/components/Menus/index.jsx
+++ client/src/components/Menus/index.jsx
@@ -1,14 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import * as S from "./style";
 
 import Menu from "../Menu";
-import MenuData from "../../Data/Menu";
+
+import 꼬깔콘 from "../../assets/Img/Menu/꼬깔콘.png";
+import 도리토스 from "../../assets/Img/Menu/도리토스.png";
+import 밀키스 from "../../assets/Img/Menu/밀키스.png";
+import 새우깡 from "../../assets/Img/Menu/새우깡.png";
+import 신짱 from "../../assets/Img/Menu/신짱.png";
+import 양파링 from "../../assets/Img/Menu/양파링.png";
+import 칠성사이다 from "../../assets/Img/Menu/칠성사이다.png";
+import 코카콜라 from "../../assets/Img/Menu/코카콜라.png";
+import 파워에이드 from "../../assets/Img/Menu/파워에이드.png";
+import 포카리스웨트 from "../../assets/Img/Menu/포카리스웨트.png";
 
 const Menus = () => {
+  const [menuData, setMenuData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://10.129.57.197:5000/snacks");
+        const data = response.data;
+
+        const menuData = data.map((item, index) => ({
+          MenuCount: item.MenuCount,
+          MenuId: item.MenuId,
+          MenuName: item.MenuName,
+          MenuPrice: item.MenuPrice,
+          MenuImg: getMenuImg(index),
+        }));
+
+        setMenuData(menuData);
+      } catch (error) {
+        console.error("데이터를 가져오는 도중 오류가 발생했습니다:", error);
+        setMenuData([]);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const getMenuImg = (index) => {
+    switch (index) {
+      case 0:
+        return 새우깡;
+      case 1:
+        return 꼬깔콘;
+      case 2:
+        return 도리토스;
+      case 3:
+        return 양파링;
+      case 4:
+        return 신짱;
+      case 5:
+        return 포카리스웨트;
+      case 6:
+        return 파워에이드;
+      case 7:
+        return 코카콜라;
+      case 8:
+        return 칠성사이다;
+      case 9:
+        return 밀키스;
+      default:
+        return null;
+    }
+  };
+
   return (
     <S.Layout>
-      {MenuData.map((item, index) => (
+      {menuData.map((item, index) => (
         <Menu
           key={index}
           MenuImg={item.MenuImg}
